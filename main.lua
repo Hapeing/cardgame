6f6a251bec5613f8ad7e3895e04fa9baa52409d2
Hapeing <hampushallstrom@hotmail.com>
2020-04-15 10:50:29 +0200

    fulscreen and esc to close

--- main.lua
+++ main.lua
@@ -6,10 +6,12 @@ function love.load()
     screen.x = 1600
     screen.y = 1000
 
-    love.window.setMode(screen.x, screen.y)
+    --love.window.setMode(screen.x, screen.y)
 
     lg = love.graphics
     lm = love.mouse
+
+    love.window.setFullscreen(true, "desktop")
 
     Game = Game:new()
 
@@ -18,8 +20,17 @@ end
 
 function love.update(dt)
 
+    if love.keyboard.isDown("escape") then
+        love.window.close()
+        love.event.quit(0)
+    end
+
     Game:update(dt)
 
+end
+
+function love.quit()
+    print("Bye!")
 end
 
 function love.draw()
